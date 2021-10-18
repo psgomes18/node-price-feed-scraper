3470f91da31d443b7662fd042ce51739696e81ce
Paulo Sérgio <65420551+psgomes18@users.noreply.github.com>
2021-10-18 15:12:52 -0300

    adding the files

---
+++ index.js
@@ -0,0 +1,78 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+async function getPriceFeed() {
+    try {
+        const siteUrl = 'https://coinmarketcap.com/';
+        const data = await axios({
+            method: 'GET',
+            url: siteUrl
+        });
+
+        const $ = cheerio.load(data);
+        const elemSelector = '#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr';
+
+        const keys = [
+            'rank',
+            'name',
+            'price',
+            '24h',
+            '7d',
+            'marketCap',
+            'volume',
+            'circulatingSupply'
+        ];
+
+        const coinArr = [];
+
+
+        $(elemSelector).each((parentIdx, parentEl) => {
+            let keyIdx = 0;
+            const coinObj = {};
+            if (parentIdx <= 9) {
+                $(parentEl).children().each((childIdx, childEl) => {
+                    let tdValue = $(childEl).text();
+
+                    if (keyIdx === t || keyIdx === 6) {
+                        tdValue = $('p:first-child', $(childEl).html()).text();
+                    }
+
+                    if (tdValue) {
+                        coinObj[keys[keyIdx]] = tdValue;
+
+                        keyIdx++;
+                    };
+                });
+                
+                coinArr.push(coinObj);
+            };
+        });
+        return coinArr;
+    } catch (err) {
+        console.error(err);
+    };
+};
+
+const app = express();
+
+app.get('/api/price-feed', async (req, res) => {
+    try {
+        const priceFeed = await getPriceFeed();
+
+        return res.status(200).json({
+            result: priceFeed,
+            
+        });
+    } catch (err) {
+        return res.status(500).json({
+            err: err.toString(),
+        });
+    };
+});
+
+app.listen(3000, () => {
+    console.log("Running on localhost:3000");
+});
+
+
